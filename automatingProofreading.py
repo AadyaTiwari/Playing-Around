c572f01462f413dd1b700c8711440e3477fa40a5
Aadya Tiwari <71808982+AadyaTiwari@users.noreply.github.com>
2023-11-08 17:30:03 +0530

    Update automatingProofreading.py
    deleting

--- automatingProofreading.py
+++ automatingProofreading.py
@@ -1,10 +1,1 @@
-import lmproof
-def proofread(text):
-    proofread = lmproof.load("en")
-    correction = proofread.proofread(text)
-    print("Original: {}".format(text))
-    print("Correction: {}".format(correction))
-    
-string = open("ProofReadingText.txt", "r")
 
-proofread(string)
