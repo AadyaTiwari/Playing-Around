6e9571365ce2f0f8f77591ac9b996661a2a7966f
Aadya Tiwari <adya.mala@gmail.com>
2023-11-10 00:24:57 +0530

    Update InternetSpeedTester.py
    adding the code and testing it too

--- InternetSpeedTester.py
+++ InternetSpeedTester.py
@@ -1,1 +1,15 @@
+import speedtest as st
+server = st.Speedtest()
+server.get_best_server
 
+down = server.download()
+down = down/1000000
+
+print(f"Download Speed is {down} Mbps")
+
+up = server.upload()
+up = up/1000000
+print(f"Upload speed is {up} Mbps")
+
+ping = server.results.ping
+print(f"Ping Speed is {ping}")
